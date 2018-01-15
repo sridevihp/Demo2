8090b362ba63b90ca4b97d871f62419766aa923a
Prashanth Holla <hp.sridevi19@gmail.com>
2018-01-15 18:10:09 +0530

    sample

---
+++ src/gitDemostration/FirstGit.java
@@ -0,0 +1,12 @@
+package gitDemostration;
+
+public class FirstGit {
+
+	public static void main(String[] args) {
+
+		System.out.println("***********Welcome to GIT!!!*******");
+		System.out.println("changes");
+
+	}
+
+}
